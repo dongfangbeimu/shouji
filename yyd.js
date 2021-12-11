7f0c2a5b870d52b90b82624bbe567cc72e28b18d
dongfangbeimu <95337470+dongfangbeimu@users.noreply.github.com>
2021-12-11 17:07:47 +0800

    Update yyd.js

--- yyd.js
+++ yyd.js
@@ -13,9 +13,9 @@ export yddCookie='账号1的Authorization#device#UA@账号2的Authorization#devi
 V2P重写：打开APP即可获取CK，没有的话点一下我的页面或者赚钱页面
 [task_local]
 #阅多多
-0-59/15 8-23 * * * https://raw.githubusercontent.com/leafxcy/JavaScript/main/ydd.js, tag=阅多多, enabled=true
+0-59/15 8-23 * * * https://raw.githubusercontent.com/dongfangbeimu/shouji/main/yyd.js, tag=阅多多, enabled=true
 [rewrite_local]
-https://yuekandian.yichengw.cn/api/v1/member/profile url script-request-header https://raw.githubusercontent.com/leafxcy/JavaScript/main/ydd.js
+https://yuekandian.yichengw.cn/api/v1/member/profile url script-request-header https://raw.githubusercontent.com/dongfangbeimu/shouji/main/yyd.js
 [MITM]
 hostname = yuekandian.yichengw.cn
 */
